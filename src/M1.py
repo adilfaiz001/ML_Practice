a3a03fb5da9fbf0d1abff135e80b9372e93a897e
Adil Faiz <adilfaiz01@live.com>
2017-02-03 02:08:21 +0530

    M1 Problem

--- src/M1.py
+++ src/M1.py
@@ -4,3 +4,15 @@ Created on Feb 3, 2017
 @author: Rahil
 '''
 #Hello World Problem-Machine learning_curve
+'''
+Supervised Machine Learning
+        Training Data--->Train Classifier--->Make Predictons
+'''
+
+from sklearn import tree
+features=[[140,1],[130,1],[170,0],[160,0]]
+labels=[1,1,0,0]
+clf=tree.DecisionTreeClassifier()
+clf.fit(features,labels)
+print clf.predict([[145,1]])
+
