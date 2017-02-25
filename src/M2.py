5801e36b1c5be9631580210eb9f2f318c81ae8cf
Adil Faiz <adilfaiz01@live.com>
2017-02-25 23:44:22 +0530

    Decision Tree-Iris Dataset

--- src/M2.py
+++ src/M2.py
@@ -3,3 +3,14 @@ Created on Feb 15, 2017
 
 @author: Rahil
 '''
+#Decision Tree-Iris Example Code
+
+## 1-Import Dataset 
+from sklearn.datasets import load_iris
+iris=load_iris()
+print iris.feature_names
+print iris.target_names
+print iris.data[0]
+print iris.target[0]
+for i in range(len(iris.target)):
+    print "Example %d: label %s, features %s" % (i,iris.data[i],iris.target[i])
