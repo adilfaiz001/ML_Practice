b443aa2874c202a1b98e565dab246366ab7fc39c
Adil Faiz <adilfaiz01@live.com>
2018-07-12 23:45:00 +0530

    Matrix Implementation of linear_reg

--- regression_algorithms/linear_reg2.py
+++ regression_algorithms/linear_reg2.py
@@ -2,3 +2,22 @@
 **Numpy implementation**
 
 '''
+
+import pandas as pd
+import numpy as np 
+from csv import reader
+
+def load_data(filename):
+    dataset = list()
+    with open(filename, 'r') as file:
+        csv_reader = reader(file)
+        for row in csv_reader:
+            if not row:
+                continue
+            dataset.append(row[0].split(','))
+    dataset = np.array(dataset)
+    print dataset.shape
+    return dataset
+
+    
+load_data('winequality-white.csv')
